5bde6b915052905eee42336d4a7e4516dab56005
Tony Nguyen <anhhao1999@gmail.com>
2020-03-09 22:18:03 -0400

    data

---
+++ data/semeval2020_task1_eng/README.md
@@ -0,0 +1,104 @@
+# English Test Data for SemEval 2020 Task 1: Unsupervised Lexical Semantic Change Detection
+
+- - -
+Siehe unten für die deutsche Version.
+- - -
+
+### Type
+
+Corpus, Dataset
+
+### Authors
+
+Dominik Schlechtweg, Haim Dubossarsky, Simon Hengchen, Barbara McGillivray, Nina Tahmasebi
+
+### Description
+
+This data collection contains the English test data for [SemEval 2020 Task 1: Unsupervised Lexical Semantic Change Detection](https://competitions.codalab.org/competitions/20948):
+
+- a English text corpus pair (`corpus1/`, `corpus2/`)
+- 37 lemmas which have been annotated for their lexical semantic change between the two corpora (`targets.txt`)
+
+__Corpus 1__
+
+- based on: [CCOHA](https://www.ims.uni-stuttgart.de/forschung/ressourcen/korpora/ccoha/) / [COHA](https://www.english-corpora.org/coha/)
+- language: English
+- time covered: 1810-1860
+- size: ~6 million tokens
+- format: lemmatized, sentence length > 9 (before removal of punctuation), no punctuation, sentences randomly shuffled
+- encoding: UTF-8
+- note: targets have been concatenated with their broad POS tag ("target_pos"); sentences are split at replacement tokens (10 x "@") and replacement tokens are removed
+
+__Corpus 2__
+
+- based on: [CCOHA](https://www.ims.uni-stuttgart.de/forschung/ressourcen/korpora/ccoha/) / [COHA](https://www.english-corpora.org/coha/)
+- language: English
+- time covered: 1960-2010
+- size: ~6 million tokens
+- format: lemmatized, sentence length > 9 (before removal of punctuation), no punctuation, sentences randomly shuffled
+- encoding: UTF-8
+- note: targets have been concatenated with their broad POS tag ("target_pos"); sentences are split at replacement tokens (10 x "@") and replacement tokens are removed
+
+Find more information on the data and SemEval2020 Task 1 in the paper referenced below.
+
+The creation of the data was supported by the CRETA center and the CLARIN-D grant funded by the German Ministry for Education and Research (BMBF).
+
+### Reference
+
+Dominik Schlechtweg, Barbara McGillivray, Simon Hengchen, Haim Dubossarsky and Nina Tahmasebi. [SemEval 2020 Task 1: Unsupervised Lexical Semantic Change Detection](https://competitions.codalab.org/competitions/20948). To appear in SemEval@COLING2020.
+
+### Download
+
+The resources are [freely available for education, research and other non-commercial purposes](https://www2.ims.uni-stuttgart.de/data/sem-eval-ulscd/semeval2020_ulscd_eng.zip).
+
+- - -
+
+# Englische Testdaten für SemEval 2020 Task 1: Unsupervised Lexical Semantic Change Detection
+
+### Typ
+
+Korpus, Datensatz
+
+### Autoren
+
+Dominik Schlechtweg, Haim Dubossarsky, Simon Hengchen, Barbara McGillivray, Nina Tahmasebi
+
+### Beschreibung
+
+Diese Datensammlung enthält die Testdaten für [SemEval 2020 Task 1: Unsupervised Lexical Semantic Change Detection](https://competitions.codalab.org/competitions/20948):
+
+- ein englisches Textkorpuspaar (`corpus1/`, `corpus2/`)
+- 37 Lemmata, die bezüglich ihres Bedeutungswandels zwischen den beiden Korpora annotiert wurden (`targets.txt`)
+
+__Corpus 1__
+
+- basiert auf: [CCOHA](https://www.ims.uni-stuttgart.de/forschung/ressourcen/korpora/ccoha/) / [COHA](https://www.english-corpora.org/coha/)
+- Sprache: Englisch
+- Zeitperiode: 1810-1860
+- Größe: ~6 Millionen Token
+- Format: lemmatisiert, Sätzlänge > 9 (vor Satzzeichenentfernung), keine Satzzeichen, Sätze wurden zufällig gemischt
+- Hinweis: Zielwörter (targets) wurden mit ihrem allgemeinen POS-Tag konkateniert ("target_pos"); Sätze wurden an Ersetzungs-Token (10 x "@") getrennt und Ersetzungs-Token wurden entfernt
+
+__Corpus 2__
+
+- basiert auf: [CCOHA](https://www.ims.uni-stuttgart.de/forschung/ressourcen/korpora/ccoha/) / [COHA](https://www.english-corpora.org/coha/)
+- Sprache: Englisch
+- Zeitperiode: 1960-2010
+- Größe: ~6 Millionen Token
+- Format: lemmatisiert, Sätzlänge > 9 (vor Satzzeichenentfernung), keine Satzzeichen, Sätze wurden zufällig gemischt
+- Textkodierung: UTF-8
+- Hinweis: Zielwörter (targets) wurden mit ihrem allgemeinen POS-Tag konkateniert ("target_pos"); Sätze wurden an Ersetzungs-Token (10 x "@") getrennt und Ersetzungs-Token wurden entfernt
+
+Weitere Informationen zu den Daten und zu SemEval2020 Task 1 finden Sie in dem unten zitierten Papier.
+
+Die Erstellung der Daten wurde unterstützt durch das CRETA-Zentrum und das CLARIN-D-Projekt gefördert durch das Bundesministerium für Bildung und Forschung (BMBF).
+
+### Referenz
+
+Dominik Schlechtweg, Barbara McGillivray, Simon Hengchen, Haim Dubossarsky and Nina Tahmasebi. [SemEval 2020 Task 1: Unsupervised Lexical Semantic Change Detection](https://competitions.codalab.org/competitions/20948). To appear in SemEval@COLING2020.
+
+### Download
+
+Die Ressourcen sind [frei verfügbar für Lehre, Forschung sowie andere nicht-kommerzielle Zwecke](https://www2.ims.uni-stuttgart.de/data/sem-eval-ulscd/semeval2020_ulscd_eng.zip).
+
+- - -
---
+++ data_utils.py
@@ -0,0 +1,164 @@
+import os
+import requests
+import json
+from collections import defaultdict
+
+corpus1_path = "project/data/semeval2020_task1_eng/corpus1/lemma/ccoha1.txt"
+corpus2_path = "project/data/semeval2020_task1_eng/corpus2/lemma/ccoha2.txt"
+target_path = "project/data/semeval2020_task1_eng/targets.txt"
+
+paths = {
+    1: os.path.join(os.path.abspath("."), corpus1_path),
+    2: os.path.join(os.path.abspath("."), corpus2_path)
+}
+
+
+def open_corpus(corpus_num, as_list=True):
+    """
+    Read the corpus and return it as an IO stream.
+    """
+    data = open(paths[corpus_num])
+    return data if not as_list else data.readlines()
+
+
+def view_first_lines(corpus, num_lines):
+    """
+    Print out the first <num_lines> of the corpus.
+
+    If the corpus is not passed in as a list of sentences, the function will
+    attempt to read the corpus using .readlines().
+    """
+    if not isinstance(corpus, list):
+        corpus = corpus.readlines()
+
+    for line in corpus[:num_lines]:
+        print(line)
+
+
+def retrieve_targets():
+    """
+    Return a list of tuple of (target word, POS) that is expected.
+    """
+    pos = {
+        "nn": "noun",
+        "vb": "verb"
+    }
+
+    file = open(target_path).readlines()
+    targets = []
+    for line in file:
+        word, tag = line.strip().split("_")
+        targets.append((word, pos[tag]))
+
+    return targets
+
+
+def retrieve_definitions(target_words, save_path=None):
+    """
+    Use the Oxford Dictionaries API to retrieve associated definitions.
+    """
+    try:
+        app_id = os.environ["APP_ID"]
+        app_key = os.environ["APP_KEY"]
+    except KeyError:
+        raise KeyError("Environment variables APP_ID and APP_KEY are necessary "
+                       "to make the api calls to OED. Please set them.")
+
+    data = []
+    for word, _ in target_words:
+        url = f"https://od-api.oxforddictionaries.com/api/v2/entries/en-us/" \
+            f"{word}?fields=definitions&strictMatch=false"
+        result = requests.get(url,
+                              headers = {"app_id": app_id, "app_key": app_key})
+
+        if str(result.status_code) != "200":
+            raise RuntimeError(f"Error code {result.status_code} when getting "
+                               f"definition for word {word}, "
+                               f"error message: {result.text}")
+
+        print(f"Retrieved definition(s) for word {word} successfully!")
+        data.append(result.json())
+
+    if save_path is not None:
+        print("Saving data to JSON file.")
+        with open(save_path, "w+") as data_file:
+            json.dump(data, data_file)
+
+    return data
+
+
+def parse_senses(definition_responses, target_words, save_path=None):
+    """
+    Return a list of senses given the
+    """
+    target_words = {word: pos for word, pos in target_words}
+
+    assert isinstance(definition_responses, str) \
+           or isinstance(definition_responses, list), \
+        "Argument defintion_reponses need to either be a path to json file " \
+        "or a list of definitions"
+
+    if isinstance(definition_responses, str):
+        definition_responses = json.load(open(definition_responses))
+
+    data = {}
+
+    for item in definition_responses:
+        word = item["id"]
+        target_pos = target_words[word]
+
+        item_data = {"senses": [], "subsenses": []}
+
+        for result in item["results"]:
+            senses = get_senses_from_lexical_entries(result, target_pos)
+            item_data["senses"].extend(senses["senses"])
+            item_data["subsenses"].extend(senses["subsenses"])
+
+        data[word] = item_data
+
+    if save_path is not None:
+        print("Saving cleaned data...")
+        with open(save_path, "w+") as data_file:
+            json.dump(data, data_file)
+
+    return data
+
+
+def get_senses_from_lexical_entries(result, target_pos):
+    """
+    Helper function to get senses from the results requested for a word.
+    """
+    lexical_entries = result["lexicalEntries"]
+    all_senses_dict = {"senses": [], "subsenses": []}
+    for lex_ent in lexical_entries:
+        if lex_ent["lexicalCategory"]["id"] == target_pos:
+            for entry in lex_ent["entries"]:
+                senses = get_senses(entry)
+                all_senses_dict["senses"].extend(senses["senses"])
+                all_senses_dict["subsenses"].extend(senses["subsenses"])
+
+    return all_senses_dict
+
+
+def get_senses(lexical_entry):
+    """
+    Format and return a list of senses
+    """
+    senses_dict = defaultdict(list)
+    senses = lexical_entry["senses"] if "senses" in lexical_entry else []
+
+    for sense in senses:
+        try:
+            senses_dict["senses"].append(sense["definitions"])
+        except KeyError:
+            senses_dict["senses"].append([])
+
+        subsenses = sense["subsenses"] if "subsenses" in sense else []
+
+        for subsense in subsenses:
+            try:
+                senses_dict["subsenses"].append(subsense["definitions"])
+            except KeyError:
+                senses_dict["subsenses"].append([])
+
+    return senses_dict
